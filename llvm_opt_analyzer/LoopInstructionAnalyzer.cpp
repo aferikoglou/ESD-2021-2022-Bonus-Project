5c9b2b2c9f2264ccc434bf0fbdb0217ca7d578a1
WilliamBlaskowicz <56600649+WilliamBlaskowicz@users.noreply.github.com>
2022-06-28 17:45:05 +0300

    Advanced Tripcount Analysis

--- llvm_opt_analyzer/LoopInstructionAnalyzer.cpp
+++ llvm_opt_analyzer/LoopInstructionAnalyzer.cpp
@@ -4,41 +4,40 @@
 #include "llvm/Analysis/LoopInfo.h"
 #include "llvm/IR/DebugInfoMetadata.h"
 #include "llvm/IR/DebugInfo.h"
+#include "llvm/Analysis/ScalarEvolution.h"
+#include "llvm/Pass.h"
 #include <iostream>
 #include <fstream>
+#include <string>
 using namespace llvm;
 
 #ifndef InstrAnalysisCount 
 #define InstrAnalysisCount 67
 // Instruction 0 unused
 #endif
+
+#ifndef FILENAME_LIM
+#define FILENAME_LIM 512
+#endif 
+
 PreservedAnalyses LoopInstructionAnalyzerPass::run(Loop &L, LoopAnalysisManager &AM,
                         LoopStandardAnalysisResults &AR, LPMUpdater &U)
-{
-    bool firstInstr = true; // variable that is only true for the first instruction in a loop.
-    const ArrayRef< BasicBlock * > BBAR = L.getBlocks(); // Get all basic blocks in a loop
-    unsigned InstructionCountArr[InstrAnalysisCount],lloc=-1;
-
-    // Set array of instruction counters to zero
+{   
+    char loopFilename[FILENAME_LIM];
+    const ArrayRef< BasicBlock * > BBAR = L.getBlocks();
+    unsigned InstructionCountArr[InstrAnalysisCount],lloc=0;
+    
     for (int i=0;i<InstrAnalysisCount;i++)
         InstructionCountArr[i]=0;
 
-    for (size_t i=0;i<BBAR.size();i++) // for every basic block in the loop
-    {
-        for (const Instruction &INSTR: *BBAR[i]) // for every instruction in the basic block
-        {
-            if (firstInstr) // execute only for the first instruction in the loop
-            {
-                firstInstr = false; // if statement will be false from now on
-                
-                // WARNING: For the following line to work, code must be compiled with "-g" (debug) flag
-                if (DILocation *Loc = INSTR.getDebugLoc()) // get line location from debug info
-                    lloc = Loc->getLine(); 
-            }
-            InstructionCountArr[INSTR.getOpcode()]++; // increment counter for the current instruction opcode
-        }
-    }
-    outs() << "LoopLine:" << lloc << ""; 
+    lloc = L.getStartLoc()->getLine();
+    strcpy(loopFilename,(L.getStartLoc()->getFilename()).data());
+    for (size_t i=0;i<BBAR.size();i++)
+        for (const Instruction &INSTR: *BBAR[i])
+            InstructionCountArr[INSTR.getOpcode()]++;
+
+    
+    outs() << "Filename,"<<loopFilename<<"|"<<"LoopLine," << lloc << "|LoopLim," <<  AR.SE.getSmallConstantTripCount(&L) <<"|Instructions"; 
     for (int i=0;i<InstrAnalysisCount;i++)
         outs()<<","<<InstructionCountArr[i];
     outs()<<"\n";
